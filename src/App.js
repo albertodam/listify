b88f24e93c349f8dfa53aab132388ea0a7896b1b
javiro93 <jotarodriguez939@gmail.com>
2021-03-14 10:31:49 +0100

    performance renders

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
-import './App.css';
-import { useState } from 'react'
-import Product from './components/Product/Product'
-import  {saveList, getList}  from './services/local.storage.service';
-import Logo from './components/Logo/Logo'
+import "./App.css";
+import { useState, memo } from "react";
+import Product from "./components/Product/Product";
+import { saveList, getList } from "./services/local.storage.service";
+import Logo from "./components/Logo/Logo";
 function App() {
-
   const [products, updateProducts] = useState(getList);
-  const [newProduct, updateNewProduct] = useState();
-
-  const handlerAddProduct = (e) => {
-    e.preventDefault();
-    const newProductsAdded = [...products, { id:getId(), name: newProduct }];
-    updateProducts(newProductsAdded)
-    document.querySelector('#product-form').reset();
-    updateNewProduct('')
-    saveList(newProductsAdded)
-  }
-
-  const handlerNameProductChange = (e) => {
-    updateNewProduct(e.target.value)
-  }
+  console.log("dentro - App");
+  const handlerAddProduct = (newProduct) => {
+    const newProductsAdded = [...products, { id: getId(), name: newProduct }];
+    updateProducts(newProductsAdded);
+    document.querySelector("#product-form").reset();
+    saveList(newProductsAdded);
+  };
 
   const getId = function () {
-    return '_' + Math.random().toString(36).substr(2, 9);
+    return "_" + Math.random().toString(36).substr(2, 9);
   };
 
   return (
     <div className="App">
-    <Logo />
-      <form id="product-form" className="form-new-product" onSubmit={handlerAddProduct}>
-        <input onChange={handlerNameProductChange} />
-        <button disabled={!newProduct}>+ Añadir producto</button>
-      </form>
-      <div className="list-product">
-      {products.map(product => <Product key={product.id} name={product.name} />)}
-      </div>
+      <Logo />
+      <NewProductForm onAddProduct={handlerAddProduct} />
+      <ProductList products={products} />
     </div>
   );
 }
+
+const NewProductForm = ({ onAddProduct }) => {
+  console.log("dentro - NewProductForm");
+  const [newProduct, updateNewProduct] = useState();
+  const handlerNameProductChange = (e) => {
+    updateNewProduct(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    updateNewProduct("");
+    onAddProduct(newProduct);
+  };
+
+  return (
+    <form id="product-form" className="form-new-product" onSubmit={onSubmit}>
+      <input onChange={handlerNameProductChange} />
+      <MyButton isDisabled={!newProduct} />
+    </form>
+  );
+};
+
+const MyButton = memo(({ isDisabled }) => {
+  console.log("dentro - MyButton");
+  return <button disabled={isDisabled}>+ Añadir producto</button>;
+});
+
+const ProductList = ({ products }) => {
+  console.log("dentro - ProductList");
+  return (
+    <div className="list-product">
+      {products.map((product) => (
+        <Product key={product.id} name={product.name} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
